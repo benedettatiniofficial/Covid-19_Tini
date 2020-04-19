b22a3961800e6e5ae7ce6da406cdad6fe02701ef
B T <benedetta.tini@outlook.it>
2020-04-19 17:26:37 +0200

    Lavoro finito

--- covid-19_TIni/MainWindow.xaml.cs
+++ covid-19_TIni/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace covid_19_TIni
 {
@@ -25,9 +27,69 @@ namespace covid_19_TIni
             InitializeComponent();
         }
 
+        List<DatoCovid19> ListaCovid = new List<DatoCovid19>();
+
         private void Btn_CaricaLista_Click(object sender, RoutedEventArgs e)
         {
+            LIst_covid19.Items.Clear();
+
+            Task.Factory.StartNew(() =>
+            {
+                Carica();
+            });
 
         }
+
+        public void Carica()
+        {
+            string file = @"DatiCovid19.xml";
+            XDocument xmlDoc = XDocument.Load(file);
+            XElement xmlroot = xmlDoc.Element("root");
+            var xmlrow = xmlroot.Elements("row");
+
+            foreach (var item in xmlrow)
+            {
+                Thread.Sleep(1000);
+                XElement xmlData = item.Element("data");
+                XElement xmlStato = item.Element("stato");
+                XElement xmricoverati_con_sintomi = item.Element("ricoverati_con_sintomi");
+                XElement xmlterapia_intensiva = item.Element("terapia_intensiva");
+                XElement xmltotale_ospedalizzati = item.Element("totale_ospedalizzati");
+                XElement xmlisolamento_domiciliare = item.Element("isolamento_domiciliare");
+                XElement xmltotale_positivi = item.Element("totale_positivi");
+                XElement xmlvariazione_totale_positivi = item.Element("variazione_totale_positivi");
+                XElement xmlnuovi_positivi = item.Element("nuovi_positivi");
+                XElement xmldimessi_guariti = item.Element("dimessi_guariti");
+                XElement xmldeceduti = item.Element("deceduti");
+                XElement xmltotale_casi = item.Element("totale_casi");
+                XElement xmltamponi = item.Element("tamponi");
+                XElement xmlnote_italia= item.Element("note_it");
+                XElement xmlnote_en = item.Element("note_en");
+                
+                DatoCovid19 a = new DatoCovid19();
+                a.data = Convert.ToDateTime(xmlData.Value);
+                a.stato = xmlStato.Value;
+                a.ricoverati_con_sintomi = Convert.ToInt32(xmricoverati_con_sintomi.Value);
+                a.terapia_intensiva = Convert.ToInt32(xmlterapia_intensiva.Value);
+                a.totale_ospedalizzati = Convert.ToInt32(xmltotale_ospedalizzati.Value);
+                a.isolamento_domiciliare = Convert.ToInt32(xmlisolamento_domiciliare.Value);
+                a.totale_positivi = Convert.ToInt32(xmltotale_positivi.Value);
+                a.variazione_totale_positivi = Convert.ToInt32(xmlvariazione_totale_positivi.Value);
+                a.nuovi_positivi = Convert.ToInt32(xmlnuovi_positivi.Value);
+                a.dimessi_guariti = Convert.ToInt32(xmldimessi_guariti.Value);
+                a.deceduti = Convert.ToInt32(xmldeceduti.Value);
+                a.totale_casi = Convert.ToInt32(xmltotale_casi.Value);
+                a.tamponi = Convert.ToInt32(xmltamponi.Value);
+                a.note_it = xmlnote_italia.Value;
+                a.note_en = xmlnote_en.Value;
+                Dispatcher.Invoke(() =>
+                {
+                    LIst_covid19.Items.Add(a);
+                });
+
+            }
+
+        }
+
     }
 }
